96c49712810ad053aaf2d3b76b6f479ae5daef4d
Fahmi Djobbi <73825072+fahmidjobbi@users.noreply.github.com>
2023-02-05 14:14:24 +0100

    Add files via upload

--- src/database.py
+++ src/database.py
@@ -1,61 +1,48 @@
-
-from datetime import datetime
-import string as String 
-import random
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import string
+import random
+db = SQLAlchemy()
 
-
-db=SQLAlchemy()
 
 class User(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String(50), unique=True, nullable=False)
-    email=db.Column(db.String(50), unique=True, nullable=False)
-    password=db.Column(db.String(50), nullable=False)
-    created_at=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated_at=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    Bookmarks=db.relationship('Bookmark', backref='user', lazy=True)
-    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.Text(), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+    bookmarks = db.relationship('Bookmark', backref="user")
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return 'User>>> {self.username}'
 
 
-    
-
 class Bookmark(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    body=db.Column(db.Text, nullable=False)
-    url=db.Column(db.Text, nullable=False)
-    short_url=db.Column(db.String(3), nullable=False)
-    visits=db.Column(db.Integer, nullable=False, default=0)
-    created_at=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated_at=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    
-    
-    
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=True)
+    url = db.Column(db.Text, nullable=False)
+    short_url = db.Column(db.String(3), nullable=True)
+    visits = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+
     def generate_short_characters(self):
-        characters=String.digits+String.ascii_letters
-        picked_chars=''.join(random.choices(characters, k=3))
-        links=self.query.filter_by(short_url=picked_chars).first()
-        if links :
+        characters = string.digits+string.ascii_letters
+        picked_chars = ''.join(random.choices(characters, k=3))
+
+        link = self.query.filter_by(short_url=picked_chars).first()
+
+        if link:
             self.generate_short_characters()
         else:
             return picked_chars
-        
-    def __init__(self,**kwargs,) :
+
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.short_url=self.generate_short_characters()
-  
-    def __repr__(self):
-        return 'Bookmark>>> {self.url}'
-    
-    
 
-    
- 
-    
+        self.short_url = self.generate_short_characters()
 
-        
-        
+    def __repr__(self) -> str:
+        return 'Boomark>>> {self.url}'
